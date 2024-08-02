9b96291469ddff410230360b5a88fd5a3bdbb5ff
Andrew Georgiou <andrewgeorgiou98@outlook.com>
2024-08-03 00:30:10 +0100

    Fixed flashbang effect caused by loading dom elements before setting dark-mode
    * Initialise dark-mode background color on html before loading elements using dark-mode

--- event_listener.js
+++ event_listener.js
@@ -1,28 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const darkMode = JSON.parse(localStorage.getItem('darkMode')) || false;
+const darkMode = JSON.parse(localStorage.getItem('darkMode')) || false;
 
-    // Add no-transition class initially
-    document.body.classList.add('no-transition');
 
     if (darkMode) {
         document.body.classList.add('dark-mode');
     }
-
-    // Remove no-transition class after the content is fully loaded
-    setTimeout(() => {
-        document.body.classList.remove('no-transition');
-    }, 0);
+document.addEventListener('DOMContentLoaded', function() {
 
     const toggleButtons = document.querySelectorAll('#toggle-dark-mode');
     toggleButtons.forEach(div => {
         div.addEventListener('click', function() {
-            document.body.classList.toggle('dark-mode');
-            const isDarkMode = document.body.classList.contains('dark-mode');
+            document.documentElement.classList.toggle('dark-mode');
+            const isDarkMode = document.documentElement.classList.contains('dark-mode');
             localStorage.setItem('darkMode', JSON.stringify(isDarkMode));
         });
     });
-
-    
 });
 
 let resizeTimer;
