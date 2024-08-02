ce44f44606d60fd2a491f96bc550d553a14b0ceb
Andrew Georgiou <andrewgeorgiou98@outlook.com>
2024-08-02 20:46:07 +0100

    Fix resize shifting elements on screen

--- event_listener.js
+++ event_listener.js
@@ -22,12 +22,14 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    let resizeTimer;
+    
+});
+
+let resizeTimer;
     window.addEventListener("resize", () => {
-        document.body.classList.add("resize-animation-stopper");
+        document.body.classList.add("no-transition");
         clearTimeout(resizeTimer);
         resizeTimer = setTimeout(() => {
-            document.body.classList.remove("resize-animation-stopper");
+            document.body.classList.remove("no-transition");
         }, 400);
     });
-});
