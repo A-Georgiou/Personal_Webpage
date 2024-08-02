7e4b8932039200596ca0f5ab0f188f970e773613
Andrew Georgiou <andrewgeorgiou98@outlook.com>
2024-08-02 20:17:40 +0100

    Add local storage for dark-mode and prevent transition on page load

--- event_listener.js
+++ event_listener.js
@@ -1,8 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const darkMode = JSON.parse(localStorage.getItem('darkMode')) || false;
+
+    // Add no-transition class initially
+    document.body.classList.add('no-transition');
+
+    if (darkMode) {
+        document.body.classList.add('dark-mode');
+    }
+
+    // Remove no-transition class after the content is fully loaded
+    setTimeout(() => {
+        document.body.classList.remove('no-transition');
+    }, 0);
+
     const toggleButtons = document.querySelectorAll('#toggle-dark-mode');
-    toggleButtons.forEach(button => {
-        button.addEventListener('click', function() {
+    toggleButtons.forEach(div => {
+        div.addEventListener('click', function() {
             document.body.classList.toggle('dark-mode');
+            const isDarkMode = document.body.classList.contains('dark-mode');
+            localStorage.setItem('darkMode', JSON.stringify(isDarkMode));
         });
+    });
+
+    let resizeTimer;
+    window.addEventListener("resize", () => {
+        document.body.classList.add("resize-animation-stopper");
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(() => {
+            document.body.classList.remove("resize-animation-stopper");
+        }, 400);
     });
 });
